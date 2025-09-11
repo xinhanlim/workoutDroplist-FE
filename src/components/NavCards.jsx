22ef9786add37e2c0cc9d977ea4e8e085176268e
xinhanlim <xhxhan00@gmail.com>
2025-09-11 06:12:29 +0000

    updated navcard

--- src/components/NavCards.jsx
+++ src/components/NavCards.jsx
@@ -8,7 +8,7 @@ export default function NavCards({ title, href, image, titleColor}) {
                 <img src={image} alt={title} className="absolute inset-0 w-full h-full object-cover transition-transform duration-500 hover:scale-105 "
                 />
                 <div className="relative z-10">
-                    <h1 className="px-4 py-4 font-bold tracking-tighter text-6xl ${titleColor}" style={{ color: titleColor }}>{title}</h1>
+                    <h1 className={`px-4 py-4 font-bold tracking-tighter text-6xl ${titleColor}`} style={{ color: titleColor }}>{title}</h1>
                 </div>
             </div>
         </Link>
